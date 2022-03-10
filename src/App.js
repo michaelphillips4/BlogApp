43e56c5f237a785a3b44c748ebb338119ed72d14
michaelphillips4 <michael.phillips4@yahoo.co.uk>
2022-03-10 13:45:21 +0000

    added blog js component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
 import Music from './Components/Music/Music.js';
 import Video from './Components/Video/Video.js';
+import Blog from './Components/Blog/Blog.js';
+
+
 function App() {
   return (
-
+<>
     <div>
      <div className="panel-music">
          <h2>Music</h2>
@@ -14,22 +16,10 @@ function App() {
           <h2>Videos</h2>
           <Video />
       </div>
-
-
-
-<h2 className='clear'>Blog Posts</h2>
-
-<div className="box blogPost">
-Nullam eget scelerisque lorem. Duis et enim malesuada, facilisis ipsum non, laoreet massa. Pellentesque nec congue nulla, at venenatis lorem. Duis condimentum efficitur mi eget auctor. Etiam vehicula luctus libero vitae ultrices. Nunc aliquet maximus libero, sed interdum velit rhoncus porttitor. Nulla facilisi. Sed diam risus, rhoncus a congue sollicitudin, convallis non nulla. Sed at ultricies turpis. Phasellus a metus efficitur, vehicula risus eget, pretium risus.
-</div>   
-
-<div className="box blogPost">
-Donec luctus massa a facilisis tristique. Duis metus turpis, ornare in urna at, consectetur egestas velit. Curabitur laoreet lacus sit amet nibh venenatis hendrerit. Curabitur tempor metus eget eros facilisis, ac vulputate lectus pharetra. Sed vitae justo consectetur, accumsan metus nec, lacinia sapien. Pellentesque vitae vulputate risus, vitae pharetra purus. Vivamus sit amet consectetur lectus. Phasellus tincidunt pretium augue eget aliquam. In in pulvinar felis, quis ullamcorper leo. In mollis massa nisi. Nunc malesuada faucibus risus, a porta erat convallis sit amet.
- </div>  
-
-      </div>
-
-  );
+    </div>
+<Blog />
+</>
+);
 }
 
 export default App;
--- src/Components/Video/Video.js
+++ src/Components/Video/Video.js
@@ -1,14 +1,14 @@
 import './Video.css';
 
-let items=['PylonSong.mp3','Item 2'];
+let items=[{name:'PylonSong.mp3',display:'Pylon Song'},{name:'Alhambra.mp3',display:'Recuerdos de la Alhambra by Francisco Tárrega'}];
 
 let itemList=items.map((item,index)=>{
   return <>
   <li key={index}>
-    <h4>{item}</h4>
+    <h4>{item.display}</h4>
     <video width="320" height="240" controls>
-        <source src="movie.mp4" type="video/mp4" />
-        <source src="movie.ogg" type="video/ogg" />
+        <source src="{item.name}" type="video/mp4" />
+      
       Your browser does not support the video tag.
     </video>
   </li>
